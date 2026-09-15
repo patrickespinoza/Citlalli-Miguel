5b1afbf28b6b97e32776cbc0d9b0e134d91dfe3c
patrick espinoza <patrickespinoza38@gmail.com>
2026-09-14 21:12:57 -0600

    cambio 4

--- Nuestra-Boda/src/componentes-encabezado/portada.jsx
+++ Nuestra-Boda/src/componentes-encabezado/portada.jsx
@@ -8,7 +8,7 @@ export default function Portada() {
       <img
         src="/portada.jpg"
         alt="Citlalli y Miguel"
-        className="absolute inset-0 w-full h-full object-cover object-[center_35%]"
+        className="absolute inset-0 w-full h-full object-cover object-[40%_35%]"
       />
 
       {/* Degradado para mejorar lectura */}
